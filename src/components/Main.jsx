00f0cef70a44034ffb83959da7ecd8da8341fff2
sezgin <sezgin.akgull@gmail.com>
2022-10-22 23:22:06 +0300

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,171 @@
+import React, { useEffect, useState } from "react";
+import mailSvg from "../assets/mail.svg";
+import manSvg from "../assets/man.svg";
+import womanSvg from "../assets/woman.svg";
+import womanAgeSvg from "../assets/growing-up-woman.svg";
+import mapSvg from "../assets/map.svg";
+import phoneSvg from "../assets/phone.svg";
+import padlockSvg from "../assets/padlock.svg";
+import axios from "axios";
+
+import Footer from "../components/footer/Footer";
+import Table from "./Table";
+
+const Main = () => {
+  const [user, setUser] = useState("");
+  const [screen, setScreen] = useState({
+    screen: "",
+    value: "",
+  });
+  const [data, setData] = useState([]);
+
+  console.log("user", user);
+  console.log("screen:", screen.screen);
+  console.log("value:", screen.value);
+
+  const randomUser = async () => {
+    const url = "https://randomuser.me/api/";
+    try {
+      const { data } = await axios(url);
+      //   console.log(data.results[0]);
+      setUser(data.results[0]);
+      setScreen({
+        screen: data.results[0].name.first + " " + data.results[0].name.last,
+        value: "name",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log("user", user);
+
+  const handlerUserList = () => {
+    data.length > 0 && data.map((item) => console.log("İtem", item));
+    setData([
+      {
+        firstName: user.name.first,
+        email: user.email,
+        phone: user.phone,
+        age: user.dob.age,
+      },
+      ...data,
+    ]);
+  };
+
+  useEffect(() => {
+    randomUser();
+  }, []);
+
+  console.log("data", data);
+
+  const { dob, name, email, phone, location, login, picture, gender } = user;
+  return (
+    <main>
+      <div className="block bcg-orange"></div>
+      <div className="block">
+        <div className="container">
+          <img src={picture?.large} alt="random user" className="user-img" />
+          <p className="user-title">
+            My {screen?.value ? screen.value : "name"} is
+          </p>
+          <p className="user-value">
+            {screen?.screen ? screen.screen : name?.first + " " + name?.last}
+          </p>
+          <div className="values-list">
+            <button className="icon" data-label="name">
+              <img
+                src={gender === "male" ? manSvg : womanSvg}
+                alt="user"
+                id="iconImg"
+                onMouseEnter={() =>
+                  setScreen({
+                    screen: `${name?.first} ${name?.last}`,
+                    value: "name",
+                  })
+                }
+              />
+            </button>
+            <button className="icon" data-label="email">
+              <img
+                src={mailSvg}
+                alt="mail"
+                id="iconImg"
+                onMouseEnter={() =>
+                  setScreen({ screen: email, value: "email" })
+                }
+              />
+            </button>
+            <button className="icon" data-label="age">
+              <img
+                src={womanAgeSvg}
+                alt="age"
+                id="iconImg"
+                onMouseEnter={() =>
+                  setScreen({ screen: dob?.age, value: "age" })
+                }
+              />
+            </button>
+            <button className="icon" data-label="street">
+              <img
+                src={mapSvg}
+                alt="map"
+                id="iconImg"
+                onMouseEnter={() =>
+                  setScreen({
+                    screen: `${location?.street?.number} ${location?.street?.name}`,
+                    value: "street",
+                  })
+                }
+              />
+            </button>
+            <button className="icon" data-label="phone">
+              <img
+                src={phoneSvg}
+                alt="phone"
+                id="iconImg"
+                onMouseEnter={() =>
+                  setScreen({ screen: phone, value: "phone" })
+                }
+              />
+            </button>
+            <button className="icon" data-label="password">
+              <img
+                src={padlockSvg}
+                alt="lock"
+                id="iconImg"
+                onMouseEnter={() =>
+                  setScreen({ screen: login?.password, value: "password" })
+                }
+              />
+            </button>
+          </div>
+          <div className="btn-group">
+            <button
+              className="btn"
+              type="button"
+              onClick={() => {
+                return randomUser();
+              }}
+            >
+              new user
+            </button>
+            <button
+              className="btn"
+              type="button"
+              onClick={() => handlerUserList()}
+            >
+              add user
+            </button>
+          </div>
+
+          <Table data={data} />
+        </div>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/components/Table.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Table = ({ data }) => {
+  console.log(data);
+  return (
+    <table className="table">
+      {data.length > 0 && (
+        <thead>
+          <tr className="head-tr">
+            <th className="th">Firstname</th>
+            <th className="th">Email</th>
+            <th className="th">Phone</th>
+            <th className="th">Age</th>
+          </tr>
+        </thead>
+      )}
+
+      <tbody>
+        {data.map((item) => {
+          return (
+            <tr className="body-tr">
+              <td className="td">{item?.firstName}</td>
+              <td className="td">{item?.email}</td>
+              <td className="td">{item?.phone}</td>
+              <td className="td">{item?.age}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
